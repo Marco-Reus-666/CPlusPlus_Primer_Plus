db942d33985d3d91575ac707b32bc25ad7861a6b
Marco-Reus-666 <1796675813@qq.com>
2022-03-11 16:21:42 +0800

    1622

--- chapter8/8.1.cpp
+++ chapter8/8.1.cpp
@@ -2,8 +2,12 @@
 
 using std::cout; using std::cin; using std::endl;
 
+inline double square(double x) { return x * x;}
 int main(){
 
-    cout << "重头来过吧骚年" << endl;
+    double a,b;
+    cout << "a = " << square(5.0) << endl;
+    cout << "b = " << square(4.5 + 7.5) << endl;
+
     return 0;
 }
